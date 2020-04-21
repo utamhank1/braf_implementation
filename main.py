fa9367fb112089db6b4bc22f834aba7152394259
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-21 10:53:02 -0400

    Added requirements.txt and basic argparser implementation to run program from command line

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello, World")
+import argparse
+import os
+import sys
+
+
+def parse_arguments():
+    """
+    Function that parses user set command line arguments to pass to program.
+    :return: string
+    """
+    # Infrastructure to set up command line argument parser.
+    parser = argparse.ArgumentParser(description='Enter the path to the directory of the PIMA diabetes.csv file you '
+                                                 'wish to analyze'
+                                     )
+    parser.add_argument("-f", "--file", type=str, help='Path to the directory containing the .txt files'
+                                                       'DIRECTORY MUST NOT CONTAIN ANY SPACES IN FOLDER NAMES')
+
+    args = parser.parse_args()
+    file = args.file
+
+    # Input validation for file.
+    if not os.path.exists(file):
+        print('The file specified does not exist or is not in the current directory.')
+        sys.exit()
+
+    return file
+
+
+def main(file):
+    print(f"Hello, World the file supplied is {file}")
 
 
 if __name__ == "__main__":
-    main()
+    arguments = parse_arguments()
+    main(arguments)
