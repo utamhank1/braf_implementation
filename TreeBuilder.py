fb08e7a957844b5818498484e76c230e3657d302
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-22 17:44:07 -0400

    Added function bagger to sample k rows from dataset

--- TreeBuilder.py
+++ TreeBuilder.py
@@ -1,24 +1,13 @@
 import numpy as np
 
-class BuildTree(object):
 
-    def __init__(self, data, depth, k):
-        self.data = data
-        self.depth = depth
-        self.k = k
-
-    def get_data(self):
-        return self.data
-
-    def get_depth(self):
-        return self.depth
-
-    def get_k(self):
-        return self.k
-
-    def get_indices(self):
-        # Bootstrap a random amount of k indices
-        k_indices = np.random_choice(len(self.get_data()), self.get_k())
-        return self.get_data()[k_indices]
-
+def bagger(dataset, k):
+    """
+    Builds subsample of a larger dataset by picking k rows with replacement.
+    :param dataset: numpy array
+    :param k: User set condition for k indices to randomly select
+    :return: numpy array of the k indexes (seeds)
+    """
+    k_indices = np.random.choice(len(dataset), k)
+    return dataset[k_indices]
 
