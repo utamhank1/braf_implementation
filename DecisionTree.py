1f63c4ec9baf85fd588262df2e2005c636fdb3f8
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-23 12:34:08 -0400

    Added results counter function for DecisionTree

--- DecisionTree.py
+++ DecisionTree.py
@@ -16,6 +16,21 @@ def choose_random_features(row):
     return random.sample(range(nb_features), int(sqrt(nb_features)))
 
 
+def unique_counts(rows):
+    """
+    Counts the number of each result in the dataset.
+    :param rows: list of lists with label at the end of each inner list.
+    :return: dict of counts of occurrence of each result.
+    """
+    results = {}
+    for row in rows:
+        r = row[len(row) - 1]
+        if r not in results:
+            results[r] = 0
+        results[r] += 1
+    return results
+
+
 class DecisionTreeClassifier(object):
     class DecisionNode(object):
         def __init__(self, col=-1, value=None, results=None, tb=None, fb=None):
@@ -27,9 +42,9 @@ class DecisionTreeClassifier(object):
 
     def __init__(self, max_depth=-1, random_features=False):
         """
-               :param  max_depth: int representing max number of training splits.
-               :param  random_features: Boolean If True, use sqrt(nb features).
-                                                If not true, use all features to train and predict.
+        :param  max_depth: int representing max number of training splits.
+        :param  random_features: Boolean If True, use sqrt(nb features).
+                                If not true, use all features to train and predict.
         """
         self.root_node = None
         self.max_depth = max_depth
