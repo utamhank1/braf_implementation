be12c3b411e0d31382308adf766cd08eabab7b10
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-23 12:13:50 -0400

    Added function to generate random features from a list

--- DecisionTree.py
+++ DecisionTree.py
@@ -2,6 +2,18 @@
 """ This module contains functionality for building a decision tree with a DecisionNodeClassifier object along with
 several functions to train the classifier.
 """
+import random
+from math import log, sqrt
+
+
+def choose_random_features(row):
+    """
+    This function chooses random features in a supplied list.
+    :param row: list
+    :return: list of random features selected
+    """
+    nb_features = len(row) - 1
+    return random.sample(range(nb_features), int(sqrt(nb_features)))
 
 
 class DecisionTreeClassifier(object):
@@ -13,13 +25,15 @@ class DecisionTreeClassifier(object):
             self.tb = tb
             self.fb = fb
 
-        """
-       :param  max_depth:          Represents max number of training splits
-       :param  random_features:    If not true, use all features to train and predict. If True, use sqrt(nb features)
-       """
-
     def __init__(self, max_depth=-1, random_features=False):
+        """
+               :param  max_depth: int representing max number of training splits
+               :param  random_features: Boolean If True, use sqrt(nb features).
+                                                If not true, use all features to train and predict.
+
+        """
         self.root_node = None
         self.max_depth = max_depth
         self.features_indexes = []
         self.random_features = random_features
+
