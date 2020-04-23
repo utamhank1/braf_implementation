b2d549bcf048abad76857d108d8f4c8bd8e2233f
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-23 16:17:04 -0400

    Modularized the braf algorithm in braf_main

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import collections
 from braf_helpers import get_neighbors, calc_unique_neighbors, calculate_model_metrics
 import math
 from RandomForestGenerator import RandomForestClassifier
+import braf_main
 
 
 def parse_arguments():
@@ -115,24 +116,8 @@ def main(file):
     p = .5
     s = 100
 
-    # Step a, extract T_min minority class from training dataset.
-    T_min = training_data_minus_fold.loc[training_data_minus_fold['Outcome'] == 1].reset_index(drop=True)
-
-    # Step b, isolate "difficult areas" affecting the minority instances.
-    # For each record in T_min, create the find the k-nearest neighbors, save these nearest neighbors in T_c.
-    full_training_dataset_minus_fold = training_data_minus_fold.values
-    k_nearest_neighbors = int(math.sqrt(len(full_training_dataset_minus_fold)))
-    T_c = pd.DataFrame(calc_unique_neighbors(full_training_dataset_minus_fold, k_nearest_neighbors, T_min),
-                       columns=raw_data.columns)
-
-    # Step c, build the main random forest rf classifier from the full dataset.
-    rf = RandomForestClassifier(nb_trees=int((1 - p) * s), nb_samples=K, max_workers=4)
-
-    # Append the random forest generated from the dataset of the critical areas and specif
-    rf.fit_combined(list(full_training_dataset_minus_fold), list(T_c.values), nb_trees_2=int(s * p))
-
     # Calculate metrics from model.
-    [precision, recall, false_positive_rate, true_positive_rate] = calculate_model_metrics(training_data, model=rf)
+    [precision, recall, false_positive_rate, true_positive_rate] = braf_main.braf(training_data_minus_fold, s, p, K)
     print(f"Precision = {precision}")
     print(f"Recall = {recall}")
     print(f"FPR = {false_positive_rate}")
