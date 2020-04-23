91928b2af62f4933cee4c292ef72cf216833ac00
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-23 12:23:19 -0400

    Added function for returning randomly selected values in given features.

--- DecisionTree.py
+++ DecisionTree.py
@@ -9,8 +9,8 @@ from math import log, sqrt
 def choose_random_features(row):
     """
     This function chooses random features in a supplied list.
-    :param row: list
-    :return: list of random features selected
+    :param row: list.
+    :return: list of random features selected.
     """
     nb_features = len(row) - 1
     return random.sample(range(nb_features), int(sqrt(nb_features)))
@@ -27,13 +27,20 @@ class DecisionTreeClassifier(object):
 
     def __init__(self, max_depth=-1, random_features=False):
         """
-               :param  max_depth: int representing max number of training splits
+               :param  max_depth: int representing max number of training splits.
                :param  random_features: Boolean If True, use sqrt(nb features).
                                                 If not true, use all features to train and predict.
-
         """
         self.root_node = None
         self.max_depth = max_depth
         self.features_indexes = []
         self.random_features = random_features
+
+    def get_features_subset(self, row):
+        """
+        Returns values that are randomly selected in the given features.
+        :param row: list
+        :return: list of randomly selected values.
+        """
+        return [row[i] for i in self.features_indexes]
 
