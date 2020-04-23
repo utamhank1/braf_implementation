8399f4da0620a716b6091616a57f2b3ee5e7bab1
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-23 12:02:41 -0400

    defined __init__ function for DecisionTreeClassifer class

--- DecisionTree.py
+++ DecisionTree.py
@@ -4,11 +4,22 @@ several functions to train the classifier.
 """
 
 
-class DecisionTreeClassifier:
-    class DecisionNode:
+class DecisionTreeClassifier(object):
+    class DecisionNode(object):
         def __init__(self, col=-1, value=None, results=None, tb=None, fb=None):
             self.col = col
             self.value = value
             self.results = results
             self.tb = tb
             self.fb = fb
+
+        """
+       :param  max_depth:          Represents max number of training splits
+       :param  random_features:    If not true, use all features to train and predict. If True, use sqrt(nb features)
+       """
+
+    def __init__(self, max_depth=-1, random_features=False):
+        self.root_node = None
+        self.max_depth = max_depth
+        self.features_indexes = []
+        self.random_features = random_features
