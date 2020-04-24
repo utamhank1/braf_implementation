6eb6d90cd9320d504568d9f44fc01ddbe87dce59
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-24 16:19:43 -0400

    Tuned the K-nearest neighbor parameter to try and improve model performance

--- braf_main.py
+++ braf_main.py
@@ -7,6 +7,7 @@ import pandas as pd
 from braf_helpers import calc_unique_neighbors, calculate_model_metrics
 import math
 from RandomForestGenerator import RandomForestClassifier
+import pdb
 
 
 def braf(training_data, test_data, s, p, K):
@@ -17,10 +18,11 @@ def braf(training_data, test_data, s, p, K):
     # Step b, isolate "difficult areas" affecting the minority instances.
     # For each record in T_min, create the find the k-nearest neighbors, save these nearest neighbors in T_c.
     training_data_minus_fold_values = training_data.values
-    k_nearest_neighbors = int(math.sqrt(len(training_data_minus_fold_values)))
+    #k_nearest_neighbors = int(math.sqrt(len(training_data_minus_fold_values)))
+    k_nearest_neighbors = 2
     T_c = pd.DataFrame(calc_unique_neighbors(training_data_minus_fold_values, k_nearest_neighbors, T_min),
                        columns=training_data.columns)
-
+    print(f"len(T_c) = {len(T_c)}")
     # Step c, build the main random forest rf classifier from the full dataset.
     rf = RandomForestClassifier(nb_trees=int((1 - p) * s), nb_samples=K, max_workers=4)
 
