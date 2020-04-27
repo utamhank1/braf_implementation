6bf83e6ec036afd1d266e6e27dfe4a8598f67002
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-04-27 01:06:42 -0400

    Added comments to braf_helpers

--- braf_helpers.py
+++ braf_helpers.py
@@ -40,6 +40,13 @@ def get_neighbors(dataset, row, k_neighbors):
 
 
 def calc_unique_neighbors(full_dataset, k, T_min):
+    """
+    Calculate the unique k nearest neighbors of each row in T_min in full_dataset.
+    :param full_dataset: pandas dataframe.
+    :param k: int.
+    :param T_min: pandas dataframe.
+    :return: numpy list of unique k nearest neighbors.
+    """
     T_min_nearest_neighbors = []
 
     for i in range(0, len(T_min)):
@@ -49,21 +56,39 @@ def calc_unique_neighbors(full_dataset, k, T_min):
 
 
 def calculate_model_metrics(training_data, model):
-    len_data = 0
-    errors = 0
+    """
+    This function records precision, recall, and training_outcome probabilities for the random forest braf algorithm.
+    :param training_data: pandas dataframe.
+    :param model: RandomForestClassifier object.
+    :return: float, float, float, dict, dict representing
+    precision, recall, false_positive_rate, metrics_dict_trees, metrics_dict the later two of which are data structures
+    to hold precision, recall, training_outcomes and probabilities of those training_outcomes determined by the trees
+    in the random forest.
+    """
+    
     true_positive = 0
     true_negative = 0
     false_positive = 0
     false_negative = 0
+
+    # Get list of features from the dataset.
     features = [ft[:-1] for ft in training_data.values]
+    # Get list of values from the dataset (the last column).
     values = [ft[-1] for ft in training_data.values]
+
+    # Initialize empty data structures to hold precision, recall, training outcomes and probability values.
     metrics_dict = {'precision': [], 'recall': []}
     metrics_dict_trees = {'training_outcomes': [], 'probabilities': []}
 
     for feature, value in zip(features, values):
+
+        # Get the prediction from the model, as well as the precision and recall from the individual decision trees.
         prediction, tree_metrics, metrics = model.predict(feature, value)
         metrics_dict_trees = dict_list_appender(metrics_dict_trees, tree_metrics)
         metrics_dict = dict_list_appender(metrics_dict, metrics)
+
+        # From the prediction made by the random forest, compare it to the actual value stored in values and determine
+        # if the prediction was a false positive, true positive, true negative, or false negative.
         if prediction != value:
             errors += 1
             if prediction == 1 and value == 0:
@@ -75,6 +100,8 @@ def calculate_model_metrics(training_data, model):
         else:
             true_positive += 1
         len_data += 1
+
+    # Calculate precision, recall, and false positive rate.
     precision = true_positive / (true_positive + false_positive)
     recall = true_positive / (true_positive + false_negative)
     false_positive_rate = false_positive / (false_positive + true_negative)
